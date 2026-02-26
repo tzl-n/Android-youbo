aed3dfcd1ad1c6a61e424f09f94dc571ac611ca8
tzl <1745450232@qq.com>
2026-02-26 19:22:31 +0800

    你好

--- app/src/main/java/com/example/android_youbo/MainActivity.kt
+++ app/src/main/java/com/example/android_youbo/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            //tj
+            //qwe
         }
     }
 }
